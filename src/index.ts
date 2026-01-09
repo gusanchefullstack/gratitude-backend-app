4d7f8b6d85966f5e1f562236379db7098fd24080
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2026-01-09 18:47:40 -0500

    Add cors and express.json middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 // index.js
 import express from "express";
+import cors from "cors";
 import routerapiv1 from "#routes/index.js";
 
 const app = express();
-const port = process.env.PORT || "9001"
+const port = process.env.PORT || "9001";
 
-app.use("/api/v1/", routerapiv1)
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1/", routerapiv1);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
