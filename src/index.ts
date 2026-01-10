7b5f85e6a4a3b0e627ade075a31e968ec350d6b6
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2026-01-10 11:52:45 -0500

    Add global error handler in expressjs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // index.js
 import express from "express";
 import cors from "cors";
-import routerapiv1 from "#routes/index.js";
+import routerapiv1 from "./routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 const port = process.env.PORT || "9001";
@@ -9,6 +10,7 @@ const port = process.env.PORT || "9001";
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", routerapiv1);
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
